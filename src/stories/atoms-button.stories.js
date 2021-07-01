aa21b45d27944f456bc8c3b9abd9d97ce6058cde
s <s.yonekura@4nonome.com>
2021-07-01 18:24:14 +0900

    初期設定

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,8 @@
+import Item_button from "../components/atoms/example/button.html";
+import "../components/atoms/example/button.scss";
+
+
+
+export default{title:"Atoms/Button"};
+
+export const Example =()=> Item_button;

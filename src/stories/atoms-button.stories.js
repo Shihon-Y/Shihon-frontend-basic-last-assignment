76e49070e69f255d3d00532e5ffda9f4fb4c3271
s <s.yonekura@4nonome.com>
2021-07-17 12:41:10 +0900

    ボタン修正３

--- src/stories/atoms-button.stories.js
+++ src/stories/atoms-button.stories.js
@@ -1,8 +1,6 @@
 import Item_button from "../components/atoms/example/button.html";
 import "../components/atoms/example/button.scss";
 
+export default { title: "Atoms/Button" };
 
-
-export default{title:"Atoms/Button"};
-
-export const Example =()=> Item_button;
+export const Example = () => Item_button;

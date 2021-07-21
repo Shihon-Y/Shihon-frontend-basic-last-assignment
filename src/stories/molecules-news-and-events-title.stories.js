a9ec722d56ca1074104bb9ade61be16e964c916c
s <s.yonekura@4nonome.com>
2021-07-21 22:43:30 +0900

    ファイル作成

---
+++ src/stories/molecules-news-and-events-title.stories.js
@@ -0,0 +1,6 @@
+import Item_newsandeventstitle from "../components/molecules/news-and-events-title.html";
+import "../components/molecules/news-and-events-title.scss";
+
+export default { title: "Molecules-NewsAndEventsTitle" };
+
+export const Example = () => Item_newsandeventstitle;

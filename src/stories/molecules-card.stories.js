3e25bb8bb8df873a68ab865d8a1ea4597f6080f2
s <s.yonekura@4nonome.com>
2021-08-21 14:18:11 +0900

    ファイル作成

---
+++ src/stories/molecules-card.stories.js
@@ -0,0 +1,6 @@
+import Item_Card from "../components/molecules/Card/card.html";
+import "../components/molecules/Card/card.scss";
+
+export default { title: "Molecules-Card" };
+
+export const Example = () => Item_Card;

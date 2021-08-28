c4d1fef9f10ad5e7eef16413c74ecd165beccae6
s <s.yonekura@4nonome.com>
2021-08-29 00:09:28 +0900

    とりあえず

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Organisms_News from "../components/organisms/news.html";
+import "../components/organisms/news.scss";
+
+export default { title: "Organisms/News" };
+
+export const Example = () => Organisms_News;
